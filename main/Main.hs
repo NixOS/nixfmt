cac179bc5a63655c1cc6e6dcab96be28ad055c52
Lars Jellema <lars.jellema@gmail.com>
2019-04-21 20:37:41 +0200

    Set exit code on failure

--- main/Main.hs
+++ main/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import           Data.Text.IO as TextIO (getContents)
-import           System.IO    (hPutStr, putStr, stderr)
+import           System.Exit  (die)
 
 import           Nixfmt
 
@@ -9,5 +9,5 @@ main :: IO ()
 main = do
     contents <- TextIO.getContents
     case parse file "<stdin>" contents of
-        Left err     -> hPutStr stderr $ errorBundlePretty err
+        Left err     -> die $ errorBundlePretty err
         Right parsed -> putDocW 80 parsed
