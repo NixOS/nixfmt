550f82b2889ca3e72f55e41560dfb97db8c1ffe5
Yorick van Pelt <yorick@yorickvanpelt.nl>
2019-05-14 13:09:28 +0200

    default.nix: additional determinism, format

--- default.nix
+++ default.nix
@@ -3,14 +3,21 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-{ pkgs ? import <nixpkgs> { }
-, installOnly ? true
-}:
+{ pkgs ? import <nixpkgs> { }, installOnly ? true }:
 
 let
-  nixfmt = pkgs.haskellPackages.callCabal2nix "nixfmt" ./. {};
+  inherit (pkgs) lib;
+  regexes = [ ".*.cabal$" "^src.*" "^main.*" "^Setup.hs$" "^js.*" "LICENSE" ];
+  src = builtins.path {
+    path = ./.;
+    name = "nixfmt-src";
+    filter = path: type:
+      let relPath = lib.removePrefix (toString ./. + "/") (toString path);
+      in lib.any (re: builtins.match re relPath != null) regexes;
+  };
+  nixfmt = pkgs.haskellPackages.callCabal2nix "nixfmt" src { };
 
-in
-  if installOnly
-  then pkgs.haskell.lib.justStaticExecutables nixfmt
-  else nixfmt
+in if installOnly then
+  pkgs.haskell.lib.justStaticExecutables nixfmt
+else
+  nixfmt
