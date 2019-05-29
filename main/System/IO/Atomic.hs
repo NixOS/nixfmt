efa6fcdd19dafedf50507f2458821f8cb4297719
Kirill Elagin <kirelagin@gmail.com>
2019-05-29 16:18:21 +0300

    atomic: Ensure no rollback after commit
    In the previous implementation the action and the commit action were
    happening inside the same `catch` clause, so it was possible that an
    exception arises while still inside this clause but after commit, and so
    we would try to rollback the transaction after it was committed.
    
    The new implementation applies the rolling-back handler only to the
    action itself, so that it no longer covers commit. Also, async exceptions
    are now masked all the way through the process, including the commit
    phase, which was one other possibility of leaving some trash behind.

--- main/System/IO/Atomic.hs
+++ main/System/IO/Atomic.hs
@@ -14,7 +14,7 @@ module System.IO.Atomic
   ( withOutputFile
   ) where
 
-import Control.Exception (bracketOnError)
+import Control.Exception (mask, onException)
 import Control.Monad (when)
 import System.Directory (doesFileExist, removeFile, renameFile)
 import System.FilePath (takeDirectory, takeFileName)
@@ -36,11 +36,9 @@ withOutputFile ::
      FilePath  -- ^ Final file path
   -> (Handle -> IO a)  -- ^ IO action that writes to the file
   -> IO a
-withOutputFile path act = bracketOnError begin rollback $ \(tpath, th) -> do
+withOutputFile path act = transact begin commit rollback $ \(tpath, th) -> do
     copyAttributes (tpath, th)
-    result <- act th
-    commit (tpath, th)
-    pure result
+    act th
   where
     tmpDir = takeDirectory path
     tmpTemplate = "." <> takeFileName path <> ".atomic"
@@ -62,3 +60,25 @@ withOutputFile path act = bracketOnError begin rollback $ \(tpath, th) -> do
 
     rollback :: (FilePath, Handle) -> IO ()
     rollback (tpath, th) = hClose th *> removeFile tpath
+
+
+
+---
+-- Helpers
+--
+
+-- | A variant of @bracket@ with two different finalisation actions.
+--
+-- > bracket acquire release = transact acquire release release
+transact ::
+     IO a         -- ^ computation to run first (\"begin transaction\")
+  -> (a -> IO b)  -- ^ computation to run on success (\"commit transaction\")
+  -> (a -> IO c)  -- ^ computation to run on failure (\"rollback transaction\")
+  -> (a -> IO d)  -- ^ computation to run in-between
+  -> IO d         -- returns the value from the in-between computation
+transact begin commit rollback act =
+  mask $ \restore -> do
+    a <- begin
+    res <- restore (act a) `onException` rollback a
+    _ <- commit a
+    pure res
