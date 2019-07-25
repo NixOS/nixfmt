b9874e729ce01fb13b6139d0d2775f2c2a722e45
Yorick <yorick.vanpelt@serokell.io>
2019-07-25 18:38:34 +0200

    FIXUP: fix ghcjs maybe

--- release.nix
+++ release.nix
@@ -10,7 +10,7 @@ let
     overrides = (self: super: {
       QuickCheck = haskell.lib.dontCheck super.QuickCheck;
       tasty-quickcheck = haskell.lib.dontCheck super.tasty-quickcheck;
-      happy = dontCheck (super.callHackage "happy" "1.19.9" {});
+      happy = haskell.lib.dontCheck (super.callHackage "happy" "1.19.9" {});
     });
   });
   regexes = [ ".*.cabal$" "^src.*" "^main.*" "^Setup.hs$" "^js.*" "LICENSE" ];
