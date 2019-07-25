2e73802bd0a50c8b00aa8e7df2fd5d67a858f7b5
Yorick van Pelt <yorick@yorickvanpelt.nl>
2019-07-25 19:15:53 +0200

    override happy for ghcjs building to fix https://github.com/ghcjs/ghcjs/issues/751

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,14 @@
 import (fetchTarball {
   url = "https://github.com/NixOS/nixpkgs/archive/b5f5c97f7d67a99b67731a8cfd3926f163c11857.tar.gz";
   sha256 = "1m9xb3z3jxh0xirdnik11z4hw95bzdz7a4p3ab7y392345jk1wgm";
-}) { config = {}; overlays = []; }
+}) { config = {}; overlays = [
+       (self: super: {
+         haskell = super.haskell // {
+           packages = super.haskell.packages // {
+             ghc864 = super.haskell.packages.ghc864.extend (hself: hsuper: {
+               happy = super.haskell.lib.dontCheck (hsuper.callHackage "happy" "1.19.9" {});
+             });
+           };
+         };
+       })
+     ]; }
