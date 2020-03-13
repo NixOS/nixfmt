a9ac16802e6ca7fdd98e079043ae43c3463c684f
Lars Jellema <lars.jellema@gmail.com>
2020-03-13 16:30:54 +0100

    Clean up and fix build system

--- default.nix
+++ default.nix
@@ -3,10 +3,32 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-{ pkgs ? import ./nixpkgs.nix, installOnly ? true }:
+{ sources ? import nix/sources.nix
+, nixpkgs ? sources.nixpkgs }:
 
 let
-  inherit (pkgs) lib;
+  overlay = self: super: {
+    haskell = super.haskell // {
+      packageOverrides = self: super: {
+        nixfmt = self.callCabal2nix "nixfmt" src { };
+      };
+    };
+  };
+
+  pkgs = import nixpkgs { overlays = [ overlay ]; };
+
+  inherit (pkgs) haskell lib;
+
+  ghcjsPackages = haskell.packages.ghcjs86.override (old: {
+    overrides = (self: super: {
+      QuickCheck = haskell.lib.dontCheck super.QuickCheck;
+      tasty-quickcheck = haskell.lib.dontCheck super.tasty-quickcheck;
+      scientific = haskell.lib.dontCheck super.scientific;
+      temporary = haskell.lib.dontCheck super.temporary;
+      time-compat = haskell.lib.dontCheck super.time-compat;
+    });
+  });
+
   regexes = [ ".*.cabal$" "^src.*" "^main.*" "^Setup.hs$" "^js.*" "LICENSE" ];
   src = builtins.path {
     path = ./.;
@@ -15,9 +37,23 @@ let
       let relPath = lib.removePrefix (toString ./. + "/") (toString path);
       in lib.any (re: builtins.match re relPath != null) regexes;
   };
-  nixfmt = pkgs.haskellPackages.callCabal2nix "nixfmt" src { };
 
-in if installOnly then
-  pkgs.haskell.lib.justStaticExecutables nixfmt
-else
-  nixfmt
+in rec {
+  nixfmt = pkgs.haskellPackages.nixfmt;
+  nixfmt-static = haskell.lib.justStaticExecutables nixfmt;
+  nixfmt-deriver = nixfmt-static.cabal2nixDeriver;
+  nixfmt-js = ghcjsPackages.callCabal2nix "nixfmt" src { };
+  nixfmt-webdemo = pkgs.runCommandNoCC "nixfmt-webdemo" { } ''
+    mkdir $out
+    cp ${./js/index.html} $_/index.html
+    cp ${nixfmt-js}/bin/js-interface.jsexe/{rts,lib,out,runmain}.js $out
+    substituteInPlace $out/index.html --replace ../dist/build/js-interface/js-interface.jsexe/ ./
+  '';
+
+  nixfmt-shell = nixfmt.env.overrideAttrs (oldAttrs: {
+    buildInputs = oldAttrs.buildInputs ++ (with pkgs; [ # nixfmt: expand
+      cabal-install
+      stylish-haskell
+    ]);
+  });
+}
