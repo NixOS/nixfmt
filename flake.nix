05659c3227bdac3e4b3f0630d5a767a9e57cd192
Yorick van Pelt <yorick@yorickvanpelt.nl>
2023-02-06 13:06:34 +0100

    Use ghcjs from 22.05, since it fails to build in 22.11

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-22.05";
 
     flake-utils.url = "github:numtide/flake-utils";
 
