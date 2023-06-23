bd44978a054ce11c2bb5d5168ef03c37eb5390ad
piegames <git@piegames.de>
2023-06-23 17:09:13 +0200

    90

--- main/Main.hs
+++ main/Main.hs
@@ -43,7 +43,7 @@ data Nixfmt = Nixfmt
 
 options :: Nixfmt
 options =
-  let defaultWidth = 85
+  let defaultWidth = 90
       addDefaultHint value message =
         message ++ "\n[default: " ++ show value ++ "]"
    in Nixfmt
