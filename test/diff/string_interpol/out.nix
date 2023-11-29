91acfa07846bd949e6c4e6cf38fa6d8fbf59aa27
piegames <git@piegames.de>
2023-11-29 20:21:45 +0100

    Tests: add interpolation test

--- test/diff/string_interpol/out.nix
+++ test/diff/string_interpol/out.nix
@@ -1,6 +1,55 @@
-"${
-# a
-"${
-# b
-"${c}"}" # d
-}"
+[
+  "${
+  # a
+  "${
+  # b
+  "${c}"}" # d
+  }"
+  {
+    ExecStart = "${pkgs.openarena}/bin/oa_ded +set fs_basepath ${pkgs.openarena}/openarena-0.8.8 +set fs_homepath /var/lib/openarena ${
+        concatMapStringsSep (x: x) " " cfg.extraFlags
+      }";
+    description = "${
+        optionDescriptionPhrase (class: class == "noun" || class == "conjunction") t1
+      } or ${
+        optionDescriptionPhrase
+          (class: class == "noun" || class == "conjunction" || class == "composite")
+          t2
+      }";
+    ruleset = ''
+      table ip nat {
+        chain port_redirects {
+          type nat hook prerouting priority dstnat
+          policy accept
+
+          ${
+            builtins.concatStringsSep "\n" (
+              map
+                (
+                  e:
+                  ''
+                    iifname "${cfg.upstreamIface}" tcp dport ${
+                      builtins.toString e.sourcePort
+                    } dnat to ${e.destination}''
+                )
+                tcpPortMap
+            )
+          }
+
+          ${
+            builtins.concatStringsSep "\n" (
+              map
+                (
+                  e:
+                  ''
+                    ifname "${cfg.upstreamIface}" udp dport ${
+                      builtins.toString e.sourcePort
+                    } dnat to ${e.destination}''
+                )
+                udpPortMap
+            )
+          }
+        }
+    '';
+  }
+]
