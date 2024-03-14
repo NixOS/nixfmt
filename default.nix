0b1b75beacb7212c1f3b3a2f60cf89207f7dca61
Silvan Mosberger <silvan.mosberger@tweag.io>
2024-03-14 22:09:07 +0100

    Focus on stable Nix and use npins for dependencies
    - Simplifies the whole thing due to not having to mess with `system`, see https://github.com/NixOS/nix/issues/3843
    - Makes the lockfile _way_ smaller, see https://github.com/NixOS/nix/issues/7730

--- default.nix
+++ default.nix
@@ -1,17 +1,66 @@
-# SPDX-FileCopyrightText: 2020 Serokell <https://serokell.io/>
+# © 2019 Serokell <hi@serokell.io>
+# © 2019 Lars Jellema <lars.jellema@gmail.com>
+# © 2024 Silvan Mosberger <contact@infinisil.com>
 #
 # SPDX-License-Identifier: MPL-2.0
 
-(import
-  (
-    let
-      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-    in
-    fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
-    }
-  )
-  {
-    src = ./.;
-  }).defaultNix
+{
+  system ? builtins.currentSystem,
+  sources ? import ./npins,
+}:
+let
+  overlay = self: super: {
+    haskell = super.haskell // {
+      packageOverrides = self: super: {
+        nixfmt = self.callCabal2nix "nixfmt" src { };
+      };
+    };
+  };
+
+  pkgs = import sources.nixpkgs {
+    inherit system;
+    overlays = [ overlay (import (sources.serokell-nix + "/overlay")) ];
+    config = {};
+  };
+
+  inherit (pkgs) haskell lib;
+
+  regexes =
+    [ ".*.cabal$" "^src.*" "^main.*" "^Setup.hs$" "^js.*" "LICENSE" ];
+  src = builtins.path {
+    path = ./.;
+    name = "nixfmt-src";
+    filter = path: type:
+      let relPath = lib.removePrefix (toString ./. + "/") (toString path);
+      in lib.any (re: builtins.match re relPath != null) regexes;
+  };
+
+  build = pkgs.haskellPackages.nixfmt;
+
+in
+build // rec {
+  packages = {
+    nixfmt = build;
+    nixfmt-static = haskell.lib.justStaticExecutables packages.nixfmt;
+    nixfmt-deriver = packages.nixfmt-static.cabal2nixDeriver;
+
+    nixfmt-shell = packages.nixfmt.env.overrideAttrs (oldAttrs: {
+      buildInputs = oldAttrs.buildInputs ++ (with pkgs; [
+        # nixfmt: expand
+        cabal-install
+        stylish-haskell
+        shellcheck
+        npins
+      ]);
+    });
+
+    inherit (pkgs) reuse;
+  };
+
+  shell = packages.nixfmt-shell;
+
+  checks = {
+    hlint = pkgs.build.haskell.hlint ./.;
+    stylish-haskell = pkgs.build.haskell.stylish-haskell ./.;
+  };
+}
--- flake.nix
+++ flake.nix
@@ -2,70 +2,19 @@
 # © 2019 Lars Jellema <lars.jellema@gmail.com>
 #
 # SPDX-License-Identifier: MPL-2.0
-
 {
-  description = "A formatter for Nix code, intended to easily apply a uniform style.";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-    flake-utils.url = "github:numtide/flake-utils";
-
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-    serokell-nix = {
-      url = "github:serokell/serokell.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-  outputs = { self, nixpkgs, flake-utils, serokell-nix, ... }:
+  outputs = { flake-utils, self }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlay = self: super: {
-          haskell = super.haskell // {
-            packageOverrides = self: super: {
-              nixfmt = self.callCabal2nix "nixfmt" src { };
-            };
-          };
-        };
-
-        pkgs = import nixpkgs {
+        result = import ./default.nix {
           inherit system;
-          overlays = [ overlay serokell-nix.overlay ];
         };
-
-        inherit (pkgs) haskell lib;
-
-        regexes =
-          [ ".*.cabal$" "^src.*" "^main.*" "^Setup.hs$" "^js.*" "LICENSE" ];
-        src = builtins.path {
-          path = ./.;
-          name = "nixfmt-src";
-          filter = path: type:
-            let relPath = lib.removePrefix (toString ./. + "/") (toString path);
-            in lib.any (re: builtins.match re relPath != null) regexes;
-        };
-
       in {
-        packages = rec {
-          default = nixfmt;
-          nixfmt = pkgs.haskellPackages.nixfmt;
-          nixfmt-static = haskell.lib.justStaticExecutables nixfmt;
-          nixfmt-deriver = nixfmt-static.cabal2nixDeriver;
-
-          nixfmt-shell = nixfmt.env.overrideAttrs (oldAttrs: {
-            buildInputs = oldAttrs.buildInputs ++ (with pkgs; [
-              # nixfmt: expand
-              cabal-install
-              stylish-haskell
-              shellcheck
-            ]);
-          });
-
-          inherit (pkgs) reuse;
+        packages = result.packages // {
+          default = result;
         };
 
         apps.default = {
@@ -73,11 +22,6 @@
           program = "${self.packages.${system}.nixfmt-static}/bin/nixfmt";
         };
 
-        devShells.default = self.packages.${system}.nixfmt-shell;
-
-        checks = {
-          hlint = pkgs.build.haskell.hlint ./.;
-          stylish-haskell = pkgs.build.haskell.stylish-haskell ./.;
-        };
+        checks = result.checks;
       });
 }
