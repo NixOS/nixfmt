caebd4062b185a13513d0061b090d5160416f93f
toastal <toastal@posteo.net>
2024-04-01 15:04:52 +0700

    Refactor flake.nix to remove flake-utils
    There is no reason to be sending downstream dependencies for a for
    loop.

--- flake.nix
+++ flake.nix
@@ -1,25 +1,27 @@
 {
-
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
   outputs =
-    { flake-utils, self }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        result = import ./default.nix { inherit system; };
-      in
-      {
-        packages = result.packages // {
-          default = result;
-        };
+    { self, nixpkgs }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
 
-        apps.default = {
+      result = forAllSystems (system: import ./default.nix { inherit system; });
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          packages = result.${system}.packages;
+        in
+        packages // { default = packages.nixfmt; }
+      );
+
+      apps = forAllSystems (system: {
+        default = {
           type = "app";
-          program = "${result}/bin/nixfmt";
+          program = nixpkgs.lib.getExe self.packages.${system}.default;
         };
+      });
 
-        checks = result.checks;
-      }
-    );
+      checks = forAllSystems (system: result.${system}.checks);
+    };
 }
