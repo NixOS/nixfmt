c697c0a7a9406639acaf9a3325b896c777525261
Silvan Mosberger <silvan.mosberger@tweag.io>
2024-04-04 23:02:31 +0200

    treefmt: Ensure all Nix files are formatted

--- default.nix
+++ default.nix
@@ -43,6 +43,12 @@ let
   treefmtEval = (import sources.treefmt-nix).evalModule pkgs {
     # Used to find the project root
     projectRootFile = ".git/config";
+
+    # This uses the version from Nixpkgs instead of the local one,
+    # which would require building the package to get a development shell
+    programs.nixfmt-rfc-style.enable = true;
+    # We don't want to format the files we use to test the formatter!
+    settings.formatter.nixfmt-rfc-style.excludes = [ "test/*" ];
   };
 in
 build
