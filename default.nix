88750d8e9ee962f6a5703910aa2591f821094759
Silvan Mosberger <silvan.mosberger@tweag.io>
2024-04-19 04:08:38 +0200

    Patch treefmt to fix parallelism
    Makes it possible to use treefmt in the sync-pr.sh script without it
    being single-threaded slow

--- default.nix
+++ default.nix
@@ -11,6 +11,16 @@ let
     haskell = super.haskell // {
       packageOverrides = self: super: { nixfmt = self.callCabal2nix "nixfmt" src { }; };
     };
+
+    treefmt = super.treefmt.overrideAttrs (old: {
+      patches = [
+        # Makes it work in parallel: https://github.com/numtide/treefmt/pull/282
+        (self.fetchpatch {
+          url = "https://github.com/numtide/treefmt/commit/f596795cd24b50f048cc395866bb90a89d99152d.patch";
+          hash = "sha256-EPn+JAT3aZLSWmpdi9ULZ8o8RvrX+UFp0cQWfBcQgVg=";
+        })
+      ];
+    });
   };
 
   pkgs = import nixpkgs {
