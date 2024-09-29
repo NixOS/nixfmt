7a0cb85d4be669a2b34a74af47a6cb43cb662ac4
piegames <git@piegames.de>
2024-09-29 12:01:55 +0200

    Experiment

--- test/diff/idioms_nixos_2/out.nix
+++ test/diff/idioms_nixos_2/out.nix
@@ -1060,7 +1060,8 @@ in
             serviceConfig.User = "nextcloud";
             # On Nextcloud ≥ 26, it is not necessary to patch the database files to prevent
             # an automatic creation of the database user.
-            environment.NC_setup_create_db_user = lib.mkIf (nextcloudGreaterOrEqualThan "26") "false";
+            environment.NC_setup_create_db_user =
+              lib.mkIf (nextcloudGreaterOrEqualThan "26") "false";
           };
         nextcloud-cron = {
           after = [ "nextcloud-setup.service" ];
