040c4facd797856039fbdbbbcf02535f823d918e
Dyego Aurélio <dyegoaurelio@gmail.com>
2025-10-30 11:04:58 -0300

    Extend single-line list formatting beyond function arguments
    Previously, single-line list formatting was limited to lists within function arguments. This change extends the behavior to all lists, applying the same rules:
    - Lists with >6 items always: expand
    - Lists with >1 items and at least one non-simple item: expand
    - Otherwise: try to fit on one line

--- test/diff/idioms_lib_3/out.nix
+++ test/diff/idioms_lib_3/out.nix
@@ -293,12 +293,7 @@ rec {
     }:
     assert builtins.isInt depthLimit;
     let
-      specialAttrs = [
-        "__functor"
-        "__functionArgs"
-        "__toString"
-        "__pretty"
-      ];
+      specialAttrs = [ "__functor" "__functionArgs" "__toString" "__pretty" ];
       stepIntoAttr =
         evalNext: name: if builtins.elem name specialAttrs then id else evalNext;
       transform =
@@ -374,11 +369,7 @@ rec {
         else if isString v then
           let
             lines = filter (v: !isList v) (builtins.split "\n" v);
-            escapeSingleline = libStr.escape [
-              "\\"
-              ''"''
-              "\${"
-            ];
+            escapeSingleline = libStr.escape [ "\\" ''"'' "\${" ];
             escapeMultiline = libStr.replaceStrings [ "\${" "''" ] [ "''\${" "'''" ];
             singlelineResult =
               ''"'' + concatStringsSep "\\n" (map escapeSingleline lines) + ''"'';
